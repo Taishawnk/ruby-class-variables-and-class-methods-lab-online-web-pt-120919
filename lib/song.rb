b0d52429868f62bb9694893779a23f1def19c9c0
Taishawn King <taishawnking1@yahoo.com>
2020-01-11 21:35:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,7 +26,7 @@ class Song
  def self.genre_count
    genre_hash ={}
    @@genres.each do |gen|
-     genre_hash[genre].nil? ? genre_hash[genre] = 1 : genre_hash[genre] += 1
+     genre_hash[gen].nil? ? genre_hash[gen] = 1 : genre_hash[gen] += 1
  binding.pry
  end
  genre_hash
